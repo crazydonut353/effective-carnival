5c499b3e4900d04f6e6f31ab130b0b8352fef319
crazydonut353 <nonyponymlg@gmail.com>
2023-05-19 12:08:48 -0500

    new stuf

--- secret_code-javascript/script.js
+++ secret_code-javascript/script.js
@@ -1,3 +1,4 @@
+
 function makeid(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -9,8 +10,45 @@ function makeid(length) {
     }
     return result;
 }
-console.log(makeid(24))
+
+
+const BASE_TEXT = makeid(40);
+
+
+/**
+ * 
+ * @param {String} text 
+ */
+
 function encryptText(text) {
-    const baseText = "";
+    const baseText = BASE_TEXT;
+    let output = "";
     
+    for (let i = 0; i < text.length; i++) {
+        //charCodeAt
+        let num = baseText.charCodeAt(i%baseText.length) + text.charCodeAt(i);
+        output = `${output}${num} `;
+    }
+    
+    return output;
+}
+/**
+ * 
+ * @param {String} text 
+ */
+
+
+
+function decryptText(text) {
+    const baseText = BASE_TEXT;
+    let output = "";
+    const charCodeArray = text.split(' ').map(function(str) { return parseInt(str); });
+    var res = [];
+    for (let i = 0; i < charCodeArray.length; i++) {
+        //array.join
+        let num = charCodeArray[i] - baseText.charCodeAt(i%baseText.length);
+        res.push(String.fromCharCode(num));
+    }
+    output = res.join("");
+    return output;
 }
