7b8d9d43154b8c39d3d07de8296836da47bff8ba
crazydonut353 <nonyponymlg@gmail.com>
2023-05-19 10:35:24 -0500

    aaaaaaa

---
+++ secret_code-javascript/script.js
@@ -0,0 +1,16 @@
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+console.log(makeid(24))
+function encryptText(text) {
+    const baseText = "";
+    
+}
