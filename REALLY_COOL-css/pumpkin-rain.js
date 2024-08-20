4415dda516c2685823ce8d59eb4ebb790a1bdbcb
cheesiefw <nonyponymlg@gmail.com>
2024-08-20 06:23:26 -0500

    changes to the homepage + deleted some stuff

--- REALLY_COOL-css/pumpkin-rain.js
+++ REALLY_COOL-css/pumpkin-rain.js
@@ -1,31 +1,56 @@
+import { Images } from "./lib/BetterImage.js";
+import { Particle, ParticleCollection_RAND } from "./lib/particle.js";
+import { AudioCollection } from "./lib/BetterAudio.js";
+
+//          CFY Homepage v1.0 REL
+//
+//  This is the full version with no WebGL.
+
 // Get the canvas element
 var canvas = document.getElementById('c');
-var gl = canvas.getContext('webgl');
+var ctx = canvas.getContext('2d');
 
 canvas.height = window.screen.height;
 canvas.width = window.screen.width;
-// Check if WebGL is supported
-if (!gl) {
-  alert('WebGL is not supported on your browser.');
-}
+
+const imageAssets = new Images(['./images/mozcheese.png', './images/pumpkin.png', './images/chedcheese.png', './images/provcheese.png', './images/amercheese.png']);
+
+var particles = null;
+
+var lastTime = Date.now();
+
+async function init() {
+  await imageAssets.load();
   
-// Set up WebGL viewport
-gl.viewport(0, 0, canvas.width, canvas.height);
-
-const images = ['./images/mozcheese.png', './images/mozcheese.png', './images/mozcheese.png', './images/mozcheese.png'];
-const imageTextures = [];
-
-// Load the images and create WebGL textures
-for (let i = 0; i < images.length; i++) {
-  const image = new Image();
-  image.src = images[i];
-
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-  imageTextures.push(texture);
+  particles = new ParticleCollection_RAND(imageAssets.files);
+  
+  gameloop();
 }
+
+function gameloop() {
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  
+  if(lastTime + 100 < Date.now()) {
+    let gh = particles.pushParticle(Math.random() * canvas.width, -60, 0, 100);
+    
+    particles.particles[gh].velocity.y = 0;
+    
+    lastTime = Date.now();
+  }
+  
+  particles.particles.forEach((x,i,a)=>{
+    if(x.x>=ctx.canvas.width||x.x<=0-100||x.y>=ctx.canvas.height+100||x.y<=0-100){
+        a.splice(i,1);
+    }
+  });
+  
+  particles.updateEach((v, i, a) => {
+    v.velocity.y += .1;
+    
+    v.draw(ctx);
+  })
+  
+  requestAnimationFrame(gameloop);
+}
+
+init();
